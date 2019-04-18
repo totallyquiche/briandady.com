25cffa293e21914001423cc33441458a1aaf9610
Brian Dady <bndady@gmail.com>
2019-04-17 21:43:42 -0500

    Adds library to manage environment variables in .env files. Creates default .env file and bootstraps variables. Creates placeholder for GitHub personal access token env var.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
     // Composer autoloader
     require_once(__DIR__ . '/../vendor/autoload.php');
+
+    // Load environment variables defined in .evn
+    $dotenv = \Dotenv\Dotenv::create(__DIR__ . '/../');
+    $dotenv->load();
