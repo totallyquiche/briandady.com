352f12d14691dd7424cc9ca21befb62a303e4aac
Brian Dady <bndady@gmail.com>
2022-11-06 20:03:13 -0600

    Adds favicon

--- public/index.php
+++ public/index.php
@@ -17,6 +17,10 @@
 
         <title><?= getUserName($session_key, $github_user_name) ?></title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
         <link rel="stylesheet" href="./assets/css/style.css">
 
         <script src="https://kit.fontawesome.com/f1f2b8f857.js" crossorigin="anonymous"></script>
